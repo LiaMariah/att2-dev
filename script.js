98022400e4c60a9d098d0a736d1f584228a6aa0c
Lia <sumakiuty@gmail.com>
2025-04-10 16:33:08 -0300

    arquivos da atividade 2

---
+++ script.js
@@ -0,0 +1,11 @@
+// Variáveis
+var nome = "Maria";
+var idade = 20;
+var altura = 1.65;
+var estudante = true;
+
+// Alertas
+alert("Nome: " + nome);
+alert("Idade: " + idade);
+alert("Altura: " + altura);
+alert("É estudante? " + estudante);
